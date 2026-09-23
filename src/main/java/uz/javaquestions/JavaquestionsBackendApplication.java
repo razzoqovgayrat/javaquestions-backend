028c8e3ad03eed2c8c22216524ca512df83509dc
G'ayrat Razzoqov <razzoqovgayrat20030805@gmail.com>
2026-09-23 16:12:32 +0500

    Initial commit: Docker setup and GitHub Actions deploy

---
+++ src/main/java/uz/javaquestions/JavaquestionsBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.javaquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaquestionsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaquestionsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/javaquestions/JavaquestionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javaquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaquestionsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
